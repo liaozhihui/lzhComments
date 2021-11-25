ea8e21e9089d740493afa1a5e1e6b6561b053cbe
lzh <1581627402@qq.com>
2021-11-25 18:05:24 +0800

    代码处理

--- trainer.py
+++ trainer.py
@@ -1,5 +1,25 @@
 from torch.utils.data import RandomSampler,DataLoader
 from transformers import AdamW, get_linear_schedule_with_warmup
+import logging
+import torch
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+def save_model(opt, model, global_step):
+    output_dir = os.path.join(opt.output_dir, 'checkpoint-{}'.format(global_step))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    # take care of model distributed / parallel training
+    model_to_save = (
+        model.module if hasattr(model, "module") else model
+    )
+    logger.info(f'Saving model & optimizer & scheduler checkpoint to {output_dir}')
+    torch.save(model_to_save.state_dict(), os.path.join(output_dir, 'model.pt'))
+
 
 
 def build_optimizer_and_scheduler(opt, model, t_total):
@@ -44,15 +64,55 @@ def build_optimizer_and_scheduler(opt, model, t_total):
 
 
 
-def train(opt,model,dataset):
+def train(opt,model,train_dataset):
 
 
-    train_sampler=RandomSampler(dataset)
+    train_sampler=RandomSampler(train_dataset)
 
-    train_loader = DataLoader(dataset=dataset,
+    train_loader = DataLoader(dataset=train_dataset,
                               batch_size=opt.train_batch_size,
                               sampler=train_sampler,num_workers=0)
 
     t_total = len(train_loader) * opt.train_epochs
     optimizer, scheduler = build_optimizer_and_scheduler(opt, model, t_total)
 
+    # Train
+    logger.info("***** Running training *****")
+    logger.info(f"  Num Examples = {len(train_dataset)}")
+    logger.info(f"  Num Epochs = {opt.train_epochs}")
+    logger.info(f"  Total training batch size = {opt.train_batch_size}")
+    logger.info(f"  Total optimization steps = {t_total}")
+
+    global_step = 0
+
+    model.zero_grad()
+
+    save_steps = t_total // opt.train_epochs
+    eval_steps = save_steps
+
+    logger.info(f'Save model in {save_steps} steps; Eval model in {eval_steps} steps')
+    log_loss_steps = 20
+    avg_loss = 0.
+
+
+    for epoch in range(opt.train_epochs):
+        for step,batch_data in enumerate(train_loader):
+
+            model.train()
+
+            loss = model(**batch_data)[0]
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), opt.max_grad_norm)
+            optimizer.step()
+            model.zero_grad()
+            global_step += 1
+
+            if global_step % log_loss_steps == 0:
+                avg_loss /= log_loss_steps
+                logger.info('Step: %d / %d ----> total loss: %.5f' % (global_step, t_total, avg_loss))
+                avg_loss = 0.
+            else:
+                avg_loss += loss.item()
+
+            if global_step % save_steps == 0:
+                save_model(opt, model, global_step)
